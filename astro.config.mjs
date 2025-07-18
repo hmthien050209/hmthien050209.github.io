e9009e2569d324a39bb8870c621dbe9164425189
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-07-18 16:12:42 +0700

    feat: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc';
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), sitemap()],
   site: 'https://hmthien050209.github.io/',
   prefetch: true,
   markdown: {
